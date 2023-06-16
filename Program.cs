957681d80d0e33649a4e71c360edbeecce39dbde
“Enter <jadhavmahesh529@gmail.com>
2023-06-16 20:25:26 +0530

    Mahesh:Given null mode happy is added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+                string result = moodAnalyser.AnalyzeMood();
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am in Happy Mood");
+            string result1 = moodAnalyser1.AnalyzeMood();
+            Console.WriteLine(result1);
+
         }
     }
 }
