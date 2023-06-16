d358a55ba46aaf0b6993f0c678803e0d751781b5
“Enter <jadhavmahesh529@gmail.com>
2023-06-16 20:18:34 +0530

    Mahesh: Refactor the code using constructor code is added

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -8,22 +8,32 @@ namespace MoodAnalazer
 {
     public class MoodAnalyser
     {
-        public string HAPPYMOOD = "Happy";
-        public string SADMOOD = "Sad";
-
-        public string AnalyzeMood(string mood)
+        string message;
+        public string HAPPY = "HAPPY";
+        public string SAD = "SAD";
+        public MoodAnalyser()
         {
-           if(mood.Contains("HAPPY")) 
-           {
-                Console.WriteLine("Happy");
-                return HAPPYMOOD;
-           }
-            if (mood.Contains("SAD");
-            {
-                Console.WriteLine("Sad");
-                return SADMOOD;
-            }
+            message = "";
         }
-        
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood()
+        {
+            if (message.Contains("I am in Sad Mood"))
+            {
+                return SAD;
+            }
+            if (message.Contains("I am in Happy Mood"))
+            {
+                return SAD;
+            }
+            return null;
+        }
+
     }
+
+
 }
+
